c25d9f66542285031341fb63d8b09b1c25f678b6
bangongzi <zhbiubiu@gmail.com>
2021-10-28 21:33:49 +0800

    add separator

--- HiveQL_helper.py
+++ HiveQL_helper.py
@@ -1,16 +1,14 @@
-def shema_generator(name_str, type_str, comment_str, len1=30, len2=15, len3=12):
-  """该函数主要目的是生成表格的Schema文件"""
-  name_list = name_str.split(',')
-  type_list = type_str.split(',')
-  comment_list = comment_str.split(',')
+def shema_generator(name_str, type_str, comment_str, sep=',', len1=30, len2=15, len3=12):
+  name_list = name_str.split(sep)
+  type_list = type_str.split(sep)
+  comment_list = comment_str.split(sep)
   for i in range(len(name_list)):
-    name, type, comment = name_list[i], type_list[i], comment_list[i]
+    name, type, comment = name_list[i].strip(), type_list[i].strip(), comment_list[i].strip()
     name_part = '  ' + name + ''.join([' ' for i in range(len1 - len(name))])
     type_part = type + ''.join([' ' for i in range(len2 - len(type))])
     COMMENT_STR = 'COMMENT'
     comment_part = "{}{}'{}',".format(COMMENT_STR, ''.join([' ' for i in range(len3 - len(COMMENT_STR))]), comment)
     print(name_part + type_part + comment_part)
-    # print("  {}\t{}\tCOMMENT\t'{}'".format(name, type, comment))
 
 
 def test_shema_generator():
